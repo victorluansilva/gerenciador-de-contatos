da5222e519ddb8486f3f006cdbc3748228745ce8
pedrohpompeu <pedropompeu1909@gmail.com>
2025-11-24 22:16:43 -0300

    atualização da interface de cor(em construção)

--- src/main/java/agenda_de_contatos/service/ConfigService.java
+++ src/main/java/agenda_de_contatos/service/ConfigService.java
@@ -30,7 +30,13 @@ public class ConfigService {
     public static final String COLOR_TXT_BROWN= "#895129";
     public static final String COLOR_CIANO_TEXT = "#00FFFF";
     public static final String COLOR_BLACK_TEXT = "000000";
-    public static final String COLOR_YELLOW_TEXT = "#E6E600" ;
+    public static final String COLOR_YELLOW_TEXT = "#E6E600";
+    public static final String COLOR_DRAK_TEXT = "#2C3E50";
+    public static final String COLO_LIGHT_TEXT = "#FFFFFF";
+    public static final String COLO_DARK_TEXT = "#3C3F41";
+    public static final String COLO_BLUE_TEXT = "#4A89F3";
+    public static final String COLO_ORANGE_TEXT = "#f0ad4e";
+    public static final String COLO_BLUEDARK_TEXT = "#554af3";
 
     //COLOR BTN
     public static final String COLOR_DEFAULT_BLUE_BUTTON = "#4A89F3";
@@ -40,25 +46,36 @@ public class ConfigService {
     public static final String COLOR_PURPLE_BUTTON = "#8A2BE2";
     public static final String COLOR_MAGENTA_BUTTON = "#FF00FF";
     public static final String COLOR_BTN_BROWN = "#895129";
-    public static final String COLOR_BACKGROND_BROWN = "#895129";
-    public static final String COLOR_BACKGROUND_RED = "FF0000";
-    public static final String COLOR_BTN_RED = "#FF0000";
     public static final String COLOR_YELLOW_BUTTON = "#E6E600";
     public static final String COLOR_BLACK_BUTTON = "000000";
     public static final String COLOR_CIANO_BUTTON = "#00FFFF";
+    public static final String COLOR_DRAK_BUTTON = "#2C3E50";
+    public static final String COLOR_LIGHT_BUTTON = "#FFFFFF";
+    public static final String COLOR_DARK_BUTTON = "#3C3F41";
+    public static final String COLOR_BLUEDARK_BUTTON = "#554af3";
+    public static final String COLOR_BTN_RED = "#FF0000";
+    public static final String COLOR_BTN_PINK = "#FFC0CB";
+
 
     //COLOR BG
+    public static final String COLOR_DEFAULT_BLUE_BG = "#4A89F3";
+    public static final String COLOR_BACKGROND_BROWN = "#895129";
+    public static final String COLOR_BACKGROUND_RED = "FF0000";
     public static final String COLOR_BG_PURPLE = "#8A2BE2";
     public static final String COLOR_BG_MAGENTA = "#FF00FF";
     public static final String COLOR_BG_YELLOW = "#E6E600";
     public static final String COLOR_BG_NUBANK = "#D3D3D3";
-    public static final String COLOR_BG_LIGHT = "#F4F4F4";
+    public static final String COLOR_BG_LIGHT = "#FFFFFF";
     public static final String COLOR_BG_DARK = "#3C3F41";
-    public static final String COLOR_BG_BLUE = "#2C3E50";
+    public static final String COLOR_BG_BLUE = "#4A89F3";
     public static final String COLOR_BG_GREEN = "#2CFF05";
     public static final String COLOR_BG_BLACK = "000000";
     public static final String COLOR_BG_ROXONUBANK= "#820AD1";
     public static final String COLOR_BG_CIANO = "#00FFFF";
+    public static final String COLOR_BG_DRAK = "#2C3E50";
+    public static final String COLOR_BG_BLUEDARK = "#554af3";
+    public static final String COLOR_BG_PINK = "#FFC0CB";
+    public static final String COLOR_BG_ORANGE = "#f0ad4e";
 
     //COLOR SIDEBAR
     public static final String COLOR_SIDEBAR_DEFAULT = "#34495E";
@@ -67,6 +84,18 @@ public class ConfigService {
     public static final String COLOR_SIDEBAR_RED = "FF0000";
     public static final String COLOR_SIDEBAR_BROWN = "#895129";
     public static final String COLOR_SIDEBAR_CIANO = "#00FFFF";
+    public static final String COLOR_SIDEBAR_BLUEDARK = "#554af3";
+    public static final String COLOR_SIDEBAR_LIGHT = "#FFFFFF";
+    public static final String COLOR_SIDEBAR_DARK = "#3C3E50";
+    public static final String COLOR_SIDEBAR_BLACK = "#000000";
+    public static final String COLOR_SIDEBAR_ROXONUBANK = "#820AD1";
+    public static final String COLOR_SIDEBAR_DRAK = "#2C3E50";
+    public static final String COLOR_SIDEBAR_BLUE =  "#4A89F3";
+    public static final String COLOR_SIDEBAR_YELLOW = "#E6E600";
+    public static final String COLOR_SIDEBAR_GREEN = "#2CFF05";
+    public static final String COLOR_SIDEBAR_NUBANK = "#D3D3D3";
+    public static final String COLOR_SIDEBAR_PINK = "#FFC0CB";
+
 
 
     public static final String COLOR_TEXT_BG_ROXONUBANK= "#820AD1";
